da84cb2abccb312f9e1764fd95514adef3d95eb8
Jimin <yangjim@umich.edu>
2022-06-24 00:28:39 -0400

    Constructor, Display Done

--- tf.h
+++ tf.h
@@ -15,18 +15,39 @@ Operations: Muiltiplying, addition, subtraction, Inverse Laplace, and Laplace
 */
 class trans_func {
     private:
-        vector< vector<int> > tf(2,vector<int>);
+        vector< vector<int> > tf;
 
     public:
-        trans_func(const vector<int> chara){
-
+        trans_func(const vector<int> & numerator){ 
+            tf.push_back(numerator);
+            tf.push_back({1});
         }
+
+        trans_func(const vector<int> & numerator, const vector<int> & denominator){ 
+            tf.push_back(numerator);
+            tf.push_back(denominator);
+        }
+
         trans_func operator/ (trans_func & a, trans_func & b){
 
         }
-        print(){
-            for(int i = 0; i < 2; i++){
 
+        trans_func operator* (trans_func & a, trans_func & b){
+
+        }
+        
+        trans_func operator+ (){
+
+        }
+
+
+        void Display(){
+            for(int i = 0; i < 2; i++){
+                for(int j = 0; j < tf[i].size()-1; j++){
+                    cout << tf[i][j] << "s^" << tf[i].size()-1 << "+";
+                }
+                cout << tf[i][tf[i].size()-1] << endl;
+                cout << "-------------" <<endl;
             }
         }    
 };
