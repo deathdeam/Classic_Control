b7e3e7981aa42e9dedb6a2a98faf9cafe662c7d8
Jimin <yangjim@umich.edu>
2022-06-26 00:44:59 -0400

    Fundamental test passed

--- control.c++
+++ control.c++
@@ -6,7 +6,16 @@ using namespace std;
 int main() {
     vector<int> num = {1,23,3};
     vector<int> den = {1,2,5,7,8};
-    trans_func func(num,den);
-    func.Display();
+    trans_func func1(num);
+    trans_func func2(den);
+    trans_func test1 = func1 + func2;
+    trans_func test2 = func1 / func2;
+    trans_func test3 = func1 * func2;
+    cout << "test display" <<endl;
+    
+    test1.Display();
+    
+    test2.Display();
+    test3.Display();
     return 0;
 }
--- tf.h
+++ tf.h
@@ -70,6 +70,7 @@ void trans_func::Display(){
     }    
     std::cout << numerator.back() <<std::endl;
     if(denominator.size() == 1){ 
+        std::cout << std::endl;
         return;
     }
     std::cout << "-------------" <<std::endl;
@@ -77,19 +78,20 @@ void trans_func::Display(){
         std::cout << denominator[i] << "s^" << denominator.size()-1-i << "+";
     }    
     std::cout << denominator.back() <<std::endl;
+    std::cout << std::endl;
     
 }
 
 
 std::vector<int> trans_func::vector_addition(std::vector<int> a , std::vector<int>  b){
     if(a.size() >= b.size()){
-        for(std::vector<int>::reverse_iterator i = a.rbegin(), j = b.rbegin() ; i != a.rend(), j != b.rend();i++, j++ ){
+        for(std::vector<int>::reverse_iterator i = a.rbegin(), j = b.rbegin() ; i != a.rend() && j != b.rend();i++, j++ ){
             *i += *j;
         }
         return a;
     }
     else{
-        for(std::vector<int>::reverse_iterator i = a.rbegin(), j = b.rbegin() ; i != a.rend(), j != b.rend();i++, j++ ){
+        for(std::vector<int>::reverse_iterator i = a.rbegin(), j = b.rbegin() ; i != a.rend() && j != b.rend();i++, j++ ){
             *j += *i;
         }
         return b;
@@ -99,7 +101,7 @@ std::vector<int> trans_func::vector_multiply(std::vector<int> a, std::vector<int
     std::vector<int> result(a.size()+b.size()-1, 0);
     for(int i = 0; i < a.size(); i++){
         for(int j = 0; j < b.size(); j++){
-            result[(a.size()-1 - i)+(b.size()- 1 - j)] = a[i]*b[j];
+            result[i+j] += a[i]*b[j];
         }
     }
     return result;
